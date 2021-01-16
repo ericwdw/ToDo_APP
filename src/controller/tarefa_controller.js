69f0671d6fca70c0c70d498390c8e652b72da892
ericwdw <ericabrunhosa@hotmail.com>
2021-01-16 12:36:41 -0300

    adicionando o nodemon

--- src/controller/tarefa_controller.js
+++ src/controller/tarefa_controller.js
@@ -1,6 +1,10 @@
 
 module.exports = (app) => {
     app.get('/tarefa', (req, res) => {
-        res.send('<h1>Tarefa</h1>')
+        res.send('<h1>Rota Get de Tarefa ativada: tarefa adicionada ao banco de dados</h1>')
+    })
+
+    app.post('/tarefa', (req, res) => {
+        res.send('<h1>Rota Post de Tarefa ativada: tarefa adicionada ao banco de dados</h1>')
     })
 };
--- src/controller/usuario_controller.js
+++ src/controller/usuario_controller.js
@@ -1,6 +1,10 @@
 
 module.exports = (app) => {
     app.get('/usuario', (req, res) => {
-        res.send('<h1>Usuario</h1>')
+        res.send('<h1>Rota Get de Usuario ativada: usuario adicionado ao banco de dados</h1>')
+    });
+
+    app.post('/usuario', (req, res) => {
+        res.send('<h1>Rota Post de Usuario ativada: usuario adicionado ao banco de dados</h1>')
     })
-};
+}
